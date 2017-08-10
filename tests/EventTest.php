9912901550fb97f95422295d37687d57f8bda55b
Leonardo Thibes <leonardothibes@gmail.com>
2017-08-10 11:43:55 -0300

    wip

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -9,11 +9,17 @@ class EventTest extends \PHPUnit_Framework_TestCase
     public function testBindSuccess()
     {
         $event = Event::getInstance();
-        $event->bind('event.name', function() {});
+        $event->bind('event.name', function() {
+            var_dump('TESTE 1');
+        });
 
-        $property = new \ReflectionProperty($event, 'events');
-        $property->setAccessible(true);
+        // $property = new \ReflectionProperty($event, 'events');
+        // $property->setAccessible(true);
 
-        print_r($event::$events['event.name']);
+        // $function = $event::fetch('event.name');
+        $function = $event::trigger('event.name');
+
+        // print_r(call_user_func($function));
+        // print_r(array_keys($event::fetchAll()));
     }
 }
