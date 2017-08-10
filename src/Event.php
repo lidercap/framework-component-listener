7633c062917e88b8bcd57c5878270e54418e50e0
Leonardo Thibes <leonardothibes@gmail.com>
2017-08-10 11:50:59 -0300

    wip

--- src/Event.php
+++ src/Event.php
@@ -4,20 +4,12 @@ declare(strict_types=1);
 
 namespace Lidercap\Component\Listener;
 
-use Lidercap\Core\Pattern;
-
 /**
  * Event Listener.
  */
 class Event
 {
-    use Pattern\Singleton;
     use Behavior\StrictAware;
-
-    /**
-     * @var array
-     */
-    protected static $events = [];
 
     /**
      * Adiciona um listener na fila de um evento.
@@ -28,40 +20,6 @@ class Event
     public static function bind(string $event, \Closure $callback)
     {
         self::$events[$event][] = $callback;
-    }
-
-    /**
-     * Recupera a fila de listeners inteira.
-     *
-     * @return array
-     */
-    public static function fetchAll() : array
-    {
-        return self::$events;
-    }
-
-    /**
-     * Recupera a fila de listeners de um evento em específico.
-     *
-     * @param string $event Nome do evento.
-     *
-     * @throws Exception\EventListenerException para eventos não encontrados, caso
-     *                                          o modo strict esteja ativado.
-     *
-     * @return array
-     */
-    public static function fetch(string $event) : array
-    {
-        if (!isset(self::$events[$event])) {
-            if (self::$strict) {
-                $message = sprintf('Evento não registrado: %s', $event);
-                throw new Exception\EventListenerException($message, -1);
-            }
-
-            return [];
-        }
-
-        return self::$events[$event];
     }
 
     /**
